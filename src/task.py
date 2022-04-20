97b9a4de0bcfd90a755918de5f92c632a46b1d80
lyphrowny <strukov.as@edu.spbstu.ru>
2022-04-20 23:54:03 +0300

    changed task's constructor (minimized num of lines)

--- src/task.py
+++ src/task.py
@@ -4,35 +4,8 @@ class task:
 
     # constructor
     def __init__(self, filename: Path):
-        C = list()# Costs matrix
-        openTime = list()# Town opening time
-        closeTime = list()# Town closing time
-        self.isInit = False # variable to check that parse is successful
-        with open(filename, "r") as file:
-            lines = file.read().splitlines()
-        if len(lines) < 2:# Small hardcode but okay
-            return
-        townNum = 0
-        # fill closeTime
-        times = lines.pop(-1).split()
-        townNum = len(times)
-        closeTime = [int(time) for time in times]
-        # fill openTime
-        times = lines.pop(-1).split()
-        if townNum != len(times):
-            return
-        openTime = [int(time) for time in times]
-        # fill C
-        while len(lines) > 0:
-            costs = lines.pop(0).split()
-            if townNum != len(costs):
-                return
-            C.append([int(cost) for cost in costs])
-        # check sizes
-        if len(C) != townNum:
-            return
-
-        self.isInit = True
-        self.C = C
-        self.openTime = openTime
-        self.closeTime = closeTime
+        *c, open_times, close_times = [[*map(float, line.split())] for line in open(filename, "r").readlines()]
+        assert len(c) == len(open_times) and len(c) == len(close_times), "Invalid data: different dimensions"
+        self.C = c
+        self.openTime = open_times
+        self.closeTime = close_times

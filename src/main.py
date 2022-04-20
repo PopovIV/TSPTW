b380e0783fdfe6cf8ab98b95e604fd403fd02855
lyphrowny <strukov.as@edu.spbstu.ru>
2022-04-20 18:56:32 +0300

    added closest neighbor w/ backtracking

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
 from task import task
 from antSolver import antSolver
 from exhaustive import exhaustive
+from backtrack import backtrack
 
 if __name__ == "__main__":
 
     # get task 
-    t = task("report.txt")
+    t = task("example.txt")
 
     if t.isInit is False:
         print("Error in parse")
     else:
         print(exhaustive(t))
+        print(backtrack(t))
         # solve task with ant method
         solver = antSolver(t)
         print("Ant method solution:")
